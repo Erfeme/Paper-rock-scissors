2abe03af8754d95f3510ba0125c9d435a891c857
Enod <win_1118@hotmail.com>
2022-07-23 00:45:36 -0500

    Added the playRound function, it calls the other function and saves the computer and player results in a variable, then it makes an if-else branching and decides all the outcomes, it works fine

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
 function getComputerChoice(){
-    let seleccion = Math.floor((Math.random() * 3) +1);
+    let seleccion = Math.floor((Math.random() * 3));
 
-    if (seleccion == 1){
-        return "piedra";
-    } else if(seleccion == 2){
-        return "tijeras";
-    } else if(seleccion == 3){
-        return "papel"
+    items = ["piedra","papel","tijeras"];
+    return items[seleccion]
+}
+
+function playRound(){
+
+    let playerSelection = prompt('Escribe Piedra, papel o tijeras').toLowerCase();
+
+    let computer = getComputerChoice();
+    
+    if(playerSelection == "piedra" && computer == "piedra"){
+        alert(`Empate!, la computadora ha seleccionado ${computer} y el jugador ha seleccionado ${playerSelection}`)
+    } else if(playerSelection == "piedra" && computer == "tijeras"){
+        alert(`Gana el jugador!, la computadora ha seleccionado ${computer} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "piedra" && computer == "papel"){
+        alert(`Gana la computadora!, la computadora ha seleccionado ${computer} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "papel" && computer == "piedra"){
+        alert(`Gana el jugador!, la computadora ha seleccionado ${computer} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "papel" && computer == "papel"){
+        alert(`Empate!, la computadora ha seleccionado ${computer} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "papel" && computer == "tijeras"){
+        alert(`Gana la computadora!, la computadora ha seleccionado ${computer} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "tijeras" && computer == "piedra"){
+        alert(`Gana la computadora!, la computadora ha seleccionado ${computer} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "tijeras" && computer == "papel"){
+        alert(`Gana el jugador!, la computadora ha seleccionado ${computer} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "tijeras" && computer == "tijeras"){
+        alert(`Empate!, la computadora ha seleccionado ${computer} y el jugador ha seleccionado ${playerSelection}`)
     }
 }
 
-let playerSelection = prompt('Escribe Piedra, papel o tijeras').toLowerCase();
