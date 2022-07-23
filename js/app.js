278e6ee8a04f757541dd06d1341f89ff717ae04d
Enod <win_1118@hotmail.com>
2022-07-22 23:53:59 -0500

    The getComputerChoice function now returns an string between piedra, papel o tijeras, it will suffice to get a random selection to the computer player

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 function getComputerChoice(){
-    let seleccion = Math.floor((Math.random() * 3) +1)
+    let seleccion = Math.floor((Math.random() * 3) +1);
+    
+    if (seleccion == 1){
+        return "piedra";
+    } else if(seleccion == 2){
+        return "tijeras";
+    } else if(seleccion == 3){
+        return "papel"
+    }
 }
